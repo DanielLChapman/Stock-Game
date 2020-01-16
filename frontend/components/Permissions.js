0f190d3e5e8e7d8917e4c449a9f09564b728e9f6
Daniel Chapman <danielc@my.ccsu.edu>
2020-01-16 16:22:09 -0500

    Front end permission update

--- frontend/components/Permissions.js
+++ frontend/components/Permissions.js
@@ -2,6 +2,7 @@ import { Query } from 'react-apollo';
 import Error from './ErrorMessage';
 import gql from 'graphql-tag';
 import Table from './styles/Table';
+import PropTypes from 'prop-types';
 
 const possiblePermissions = [
     'ADMIN',
@@ -37,12 +38,12 @@ const Permissions = (props) => (
                                 <th>
                                     Email
                                 </th>
-                                {possiblePermissions.map(permission => <th>{permission}</th>)}
+                                {possiblePermissions.map((permission) => <th key={permission}>{permission}</th>)}
                                 <th>Update</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {data.users.map(user => <User user={user} />)}
+                            {data.users.map(user => <UserPermissions key={user.id} user={user} />)}
                         </tbody>
                     </Table>
                 </div>
@@ -51,17 +52,44 @@ const Permissions = (props) => (
     </Query>
 )
 
-class User extends React.Component {
+class UserPermissions extends React.Component {
+    static propTypes = {
+        user: PropTypes.shape({
+            name: PropTypes.string,
+            email: PropTypes.string,
+            id: PropTypes.string,
+            permissions: PropTypes.array
+        }).isRequired
+    };
+    state = {
+        permissions: this.props.user.permissions,
+    }
+    handlePermissionChange = (e) => {
+        const checkbox = e.target;
+        //take a copy of current permissions
+        let updatedPermissions = [...this.state.permissions];
+        //figure out if we need to remove or add permission
+        if (checkbox.checked) {
+            //add it
+            updatedPermissions.push(checkbox.value);
+        } else {
+            updatedPermissions = updatedPermissions.filter(permission => permission !== checkbox.value);
+        }
+        this.setState({permissions:updatedPermissions});
+    }
     render() {
         const user = this.props.user;
         return (
             <tr>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
-                {possiblePermissions.map(permission => (
-                    <td>
+                {possiblePermissions.map((permission) => (
+                    <td key={permission}>
                         <label htmlFor={`${user.id}-permission-${permission}`}>
-                            <input type="checkbox" />
+                            <input type="checkbox" checked={this.state.permissions.includes(permission)} 
+                            value={permission}
+                            onChange={this.handlePermissionChange}
+                            />
                         </label>
                     </td>
                 ))}
