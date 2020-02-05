6828a1131601021b56b09921b926d71b7854f5c3
Daniel Chapman <danielc@my.ccsu.edu>
2020-02-05 17:13:57 -0500

    Stock Searching

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,5 +1,6 @@
 const { forwardTo } = require('prisma-binding');
 const { hasPermission } = require('../utils');
+const axios = require('axios');
 
 const Query = {
    stocks: forwardTo('db'),
@@ -24,6 +25,50 @@ const Query = {
       hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
       //query all users
       return ctx.db.query.users({}, info);
+   },
+   async stockSearch(parent, args, ctx, info) {
+      let stock = {
+         symbol: "BL",
+         message: "",
+         price: 0,
+         opening: 0
+      };
+      let user = null;
+      let apiKey = "";
+      //check if logged in
+      if (ctx.request.userId) {
+         user = await ctx.db.query.user({
+            where: { id: ctx.request.userId }
+         }, info);
+      };
+
+      //if logged in, get their api key
+      if (user) {
+         apiKey = user.apikey;
+      }
+      // else use the free one
+      else {
+         apiKey = process.env.FREEAPIKEY
+      }
+      // verify there are no special characters in symbol
+      if(/[^a-zA-Z0-9]/.test(args.symbol)) {
+         throw new Error('Invalid Symbol');
+      }
+      //make api call
+      let res = await axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${args.symbol}&interval=5min&apikey=${apiKey}`);
+      //verify stock
+      if (res.data['Error Message']) {
+         throw new Error('Possible Invalid SYMBOL, Please Double Check It Is A Real Stock Symbol');
+      }
+      
+      let recent = res.data['Global Quote'];
+      stock = {
+         symbol: args.symbol,
+         price: recent['05. price'],
+         opening: recent['02. open']
+      }
+      //return stock
+      return stock;
    }
 };
 
